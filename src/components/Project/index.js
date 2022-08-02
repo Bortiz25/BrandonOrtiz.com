f0ca517c6915f4c5ec7d1a3bd936df7fe96252e1
Brandon Ortiz <ortizbrandon140@gmail.com>
2022-08-02 17:06:44 -0500

    project page demolishion

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,16 +3,7 @@ import styles from "./Project.module.css";
 import inProgress from "../../resources/inProgress.gif";
 
 function Projects() {
-  return (
-    <div className={styles.main}>
-      <img
-        src={inProgress}
-        alt="can't load . . ."
-        className={styles.progress}
-      />
-      <h1 className={styles.message}>Page coming soon!👷</h1>
-    </div>
-  );
+  return <div className={styles.main}></div>;
 }
 
 export default Projects;
