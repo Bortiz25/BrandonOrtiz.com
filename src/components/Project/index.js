277dfd4e76f773fb6e672f86c0299668207cbdc8
Brandon Ortiz <ortizbrandon140@gmail.com>
2022-08-03 17:17:19 -0500

    project page/ work in progeress

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,9 +1,35 @@
 // function component that will render the Projects page
 import styles from "./Project.module.css";
-import inProgress from "../../resources/inProgress.gif";
+import personalWeb from "../../resources/projectspage/websiteSC.png";
+import { ReactComponent as Github } from "../../resources/projectspage/projectGithub.svg";
 
 function Projects() {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.title}>
+        <h2>Projects</h2>
+      </div>
+
+      <div className={styles.websiteSection}>
+        <div className={styles.webContainer}>
+          <a
+            href="https://github.com/Bortiz25/BrandonOrtiz.com"
+            target="_blank"
+          >
+            <div className={styles.webLayer}>
+              <Github className={styles.github} />
+            </div>
+          </a>
+          <img
+            className={styles.websiteImg}
+            src={personalWeb}
+            alt="personal website not loading . . ."
+          />
+        </div>
+        <h3 className={styles.webDescription}>Personal Website</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
