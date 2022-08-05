0a6dcf4b77d3108a642fa0768ab089295df157ab
Brandon Ortiz <ortizbrandon140@gmail.com>
2022-08-05 18:05:30 -0500

    project page first iteration

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,13 +3,15 @@ import styles from "./Project.module.css";
 import personalWeb from "../../resources/projectspage/websiteSC.png";
 import { ReactComponent as Github } from "../../resources/projectspage/projectGithub.svg";
 
+// this component will render the body of the Projects page
+// it contains image sneakpeeks to projects adn links to their github repos
 function Projects() {
   return (
     <div className={styles.main}>
       <div className={styles.title}>
         <h2>Projects</h2>
       </div>
-
+      {/* personal website section and link to github */}
       <div className={styles.websiteSection}>
         <div className={styles.webContainer}>
           <a
